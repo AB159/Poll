1e6560f516e4b7552d85ff42c69f8d8d2dcfd75a
Alex <alexander.bueso@hilliardschools.org>
2018-05-03 11:22:45 -0400

    Poll v2 and txt file
    Finished pol v2 and added fxt file

---
+++ Poll v2.py
@@ -0,0 +1,37 @@
+'''
+Poll v 2.2
+By Alex 5/1/2018
+'''
+import ast
+names = {}
+try:    
+    file = open("file.txt", "r")
+    names = file.read()
+    names = ast.literal_eval(names)
+    file.close()
+except:
+    names = {}
+
+while True:
+    name = input(str("Type in your name/Type quit to see the average "))
+    if name in names:
+        print("Already have that name")
+        continue
+    if name == "quit":
+        print("Average is " + str(sum(names.values()) / len(names)))
+        break
+    if len(name) < 1:
+        print("You have to type something!")
+        continue
+    if len(name) > 1:
+        while True:
+            answer = input(str("How many siblings do you have?"))
+            try:
+                answer = int(answer)
+                names[name] = answer
+                break
+            except:
+                print("I only take numbers!")
+    file = open("file.txt", "w")
+    file.write(str(names))
+    file.close()
