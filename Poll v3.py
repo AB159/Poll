f1a9685d5bc11f10966e1ff7d9c8f49a514df60a
Alex <alexander.bueso@hilliardschools.org>
2018-05-07 12:47:41 -0400

    Done

--- Poll v3.py
+++ Poll v3.py
@@ -1,7 +1,9 @@
 '''
 Poll v 3.0
-By Alex 5/1=4/2018
+By Alex 5//4/2018
 '''
+from tkinter import *
+import ast
 names = {}
 try:    
     file = open("file.txt", "r")
@@ -11,11 +13,31 @@ try:
 except:
     names = {}
 
-
-
-
-from tkinter import *
-import ast
+def submitInput():
+    name = textbox1.get()
+    number = textbox2.get()
+    if name ==  "":
+        messagebox.showerror("Box Title","Name can't be blank")
+        return
+    if name in names:
+        messagebox.showerror("Box Title","Name already used")
+        return
+    try:
+        number = float(number)
+    except:
+         messagebox.showerror("Box Title","Please enter a number")
+         return
+    if number < 0:
+        messagebox.showerror("Box Title","Number must be 0 or greater")
+        return
+    names[name] = number
+    messagebox.showinfo("Names", names)
+    textbox1.delete(0,END)
+    textbox2.delete(0,END)
+    file = open("file.txt", "w")
+    file.write(str(names))
+    file.close()
+    
 root = Tk()
 root.title("Poll v3")
 
@@ -27,15 +49,16 @@ textbox2 = Entry(root)
 
 textbox1.grid(row=0, column=1)
 textbox2.grid(row=1, column=1)
+def calAverage():
+    messagebox.showinfo("Title",str(len(names)) +" Responses ")
+    if len(names) > 0:
+        messagebox.showinfo(" Average", sum(names.values())/len(names))
 
-btn1 = Button(root, text= "Submit", command=submit)
+btn1 = Button(root, text= "Submit", command=submitInput)
 btn1.grid(row=2,column=0)
-btn2 = Button(root, text= "Average", command=root.destroy)
+btn2 = Button(root, text= "Average", command=calAverage)
 btn2.grid(row=2,column=1)
 btn3 = Button(root, text= "Quit", command=root.destroy)
 btn3.grid(row=2,column=2)
 
-def submit():
-    
-    if name == ""
-    
+
